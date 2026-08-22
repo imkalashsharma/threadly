587e2d8434f97889e25abfb2a10f54ca0a0e2a40
Kalash Sharma <kalash.strt@gmail.com>
2026-08-22 13:14:46 +0530

    chore: modules and DB init script
     added DB init script for docker
     added different module repos db configuration
     added common repo as a shared repo

---
+++ infrastructure/docker/postgres/init/01-create-databases.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE threadly_auth;
+CREATE DATABASE threadly_catalog;
+CREATE DATABASE threadly_cart;
+CREATE DATABASE threadly_inventory;
+CREATE DATABASE threadly_order;
+CREATE DATABASE threadly_notification;
